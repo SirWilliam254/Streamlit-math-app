442a412c6816b3c81ca2c20d6d84d09c6f1a710e
William Mburu <69644126+SirWilliam254@users.noreply.github.com>
2023-02-26 04:57:11 +0300

    Update 3_📈_AB-Test.py

--- pages/3_📈_AB-Test.py
+++ pages/3_📈_AB-Test.py
@@ -1,93 +1,68 @@
 import streamlit as st
-import pandas as pd
 import numpy as np
 import scipy.stats as stats
-import matplotlib.pyplot as plt
-import seaborn as sns
 
-def perform_ab_test(df, group_col, metric_col, alpha, test_type):
-    """
-    Perform A/B test on two groups using a specified significance level and test type.
+# Define a dictionary of distribution names and their corresponding functions
+distributions = {
+    'Beta': {'func': stats.beta, 'params': ['alpha', 'beta']},
+    'Binomial': {'func': stats.binom, 'params': ['n', 'p']},
+    'Exponential': {'func': stats.expon, 'params': ['scale']},
+    'Gamma': {'func': stats.gamma, 'params': ['k', 'theta']},
+    'Geometric': {'func': stats.geom, 'params': ['p']},
+    'Logistic': {'func': stats.logistic, 'params': ['loc', 'scale']},
+    'Log Normal': {'func': stats.lognorm, 'params': ['s', 'loc', 'scale']},
+    'Negative Binomial': {'func': stats.nbinom, 'params': ['n', 'p']},
+    'Normal': {'func': stats.norm, 'params': ['loc', 'scale']},
+    'Poisson': {'func': stats.poisson, 'params': ['mu']},
+    'T': {'func': stats.t, 'params': ['df']},
+    'Uniform': {'func': stats.uniform, 'params': ['loc', 'scale']},
+    'Weibull': {'func': stats.weibull_min, 'params': ['c', 'scale']}
+}
 
-    Parameters:
-    -----------
-    df : pandas DataFrame
-        Data containing the two groups to be compared
-    group_col : str
-        Column name representing the group variable
-    metric_col : str
-        Column name representing the metric variable
-    alpha : float
-        Significance level for the test
-    test_type : str
-        Type of test to be performed (either 'two-tail' or 'one-tail')
+# Define a function to calculate the probability density/mass function
+def calc_prob_dist(distribution, params, x, case):
+    dist = distributions[distribution]['func'](*params)
+    if case == 'p<x':
+        prob = dist.cdf(x)
+    elif case == 'p>x':
+        prob = 1 - dist.cdf(x)
+    elif case == 'p1<x<p2':
+        prob = dist.cdf(x[1]) - dist.cdf(x[0])
+    return prob
 
-    Returns:
-    --------
-    test_result : str
-        Result of the test (either 'Reject Null' or 'Fail to Reject Null')
-    test_statistic : float
-        Test statistic value
-    p_value : float
-        P-value for the test
-    test_significance : float
-        Significance level used for the test
-    """
-    # Split the data into the two groups
-    group1 = df[df[group_col] == df[group_col].unique()[0]][metric_col]
-    group2 = df[df[group_col] == df[group_col].unique()[1]][metric_col]
+# Define the Streamlit app
+def main():
+    st.title('Probability Calculator')
+    st.write('This app allows you to calculate the probability density/mass function of different distributions.')
+    
+    # Create dropdown menu to select a distribution
+    distribution = st.selectbox('Select a distribution:', list(distributions.keys()))
+    
+    # Get parameter values for the selected distribution
+    params = []
+    for param in distributions[distribution]['params']:
+        param_val = st.number_input(f'Enter value for {param}:', value=1)
+        params.append(param_val)
+    
+    # Create dropdown menu to select the case
+    case = st.radio('Select a case:', ['p<x', 'p>x', 'p1<x<p2'])
+    
+    # Get x value(s) for the probability density/mass function
+    if case == 'p<x':
+        x = st.number_input('Enter x value:', value=0.5)
+    elif case == 'p>x':
+        x = st.number_input('Enter x value:', value=0.5)
+    elif case == 'p1<x<p2':
+        x_min = st.number_input('Enter minimum x value:', value=0)
+        x_max = st.number_input('Enter maximum x value:', value=1)
+        x = [x_min, x_max]
+    
+    # Calculate the probability
+    prob = calc_prob_dist(distribution, params, x, case)
+    
+    # Display the probability
+    st.write(f'P({case}) = {prob}')
+    
 
-    # Perform the A/B test
-    if test_type == 'two-tail':
-        test_result, p_value = stats.ttest_ind(group1, group2)
-        test_significance = alpha / 2
-    elif test_type == 'one-tail':
-        test_result, p_value = stats.ttest_ind(group1, group2, alternative='greater')
-        test_significance = alpha
-    else:
-        raise ValueError("Invalid test type. Must be either 'two-tail' or 'one-tail'.")
-
-    # Determine whether to reject the null hypothesis
-    if p_value < test_significance:
-        test_result = 'Reject Null'
-    else:
-        test_result = 'Fail to Reject Null'
-
-    # Calculate the test statistic
-    if test_type == 'two-tail':
-        test_statistic = np.abs(test_result)
-    else:
-        if group1.mean() > group2.mean():
-            test_statistic = stats.t.ppf(1 - p_value, len(group1) + len(group2) - 2)
-        else:
-            test_statistic = stats.t.ppf(p_value, len(group1) + len(group2) - 2)
-
-    # Generate a plot of the distribution of the metric variable in the two groups
-    fig, ax = plt.subplots()
-    sns.histplot(df, x=metric_col, hue=group_col, kde=True, ax=ax)
-    ax.set_title(f"Distribution of {metric_col} by {group_col}")
-    st.pyplot(fig)
-
-    return test_result, test_statistic, p_value, test_significance
-
-# Set up the Streamlit app
-st.set_page_config(page_title="A/B Test Calc")
-st.title("A/B Test App")
-
-# Allow the user to upload a CSV file
-uploaded_file = st.file_uploader("Upload a CSV file", type="csv")
-if uploaded_file is not None:
-    # Load the data into a pandas DataFrame
-    df = pd.read_csv(uploaded_file)
-
-    # Allow the user to specify the input parameters
-    alpha = st.number_input("Significance level for the test", value=0.05, step=0.01)
-    test_type = st.selectbox("Type of test", options=['two-tail', 'one-tail'])
-
-# Perform the A/B test and display the results
-if st.button("Run A/B Test"):
-    result, statistic, p_value, significance = perform_ab_test(df, group_col, metric_col, alpha, test_type)
-    st.write(f"Test result: {result}")
-    st.write(f"Test statistic: {statistic}")
-    st.write(f"P-value: {p_value}")
-    st.write(f"Significance level: {significance}")
+if __name__ == '__main__':
+    main()
