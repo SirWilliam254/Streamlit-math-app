cef110e57821c5292e36768ffca75d339a878a1a
William Mburu <69644126+SirWilliam254@users.noreply.github.com>
2023-02-26 03:21:38 +0300

    Update 3_📈_AB-Test.py

--- pages/3_📈_AB-Test.py
+++ pages/3_📈_AB-Test.py
@@ -4,66 +4,65 @@ import scipy.stats as stats
 
 # Define a dictionary of distribution names and their corresponding functions
 distributions = {
-    'Beta': stats.beta,
-    'Binomial': stats.binom,
-    'Exponential': stats.expon,
-    'Gamma': stats.gamma,
-    'Geometric': stats.geom,
-    'Logistic': stats.logistic,
-    'Log Normal': stats.lognorm,
-    'Negative Binomial': stats.nbinom,
-    'Normal': stats.norm,
-    'Poisson': stats.poisson,
-    'T': stats.t,
-    'Uniform': stats.uniform,
-    'Weibull': stats.weibull_min
+    'Beta': {'func': stats.beta, 'params': ['alpha', 'beta']},
+    'Binomial': {'func': stats.binom, 'params': ['n', 'p']},
+    'Exponential': {'func': stats.expon, 'params': ['scale']},
+    'Gamma': {'func': stats.gamma, 'params': ['k', 'theta']},
+    'Geometric': {'func': stats.geom, 'params': ['p']},
+    'Logistic': {'func': stats.logistic, 'params': ['loc', 'scale']},
+    'Log Normal': {'func': stats.lognorm, 'params': ['s', 'loc', 'scale']},
+    'Negative Binomial': {'func': stats.nbinom, 'params': ['n', 'p']},
+    'Normal': {'func': stats.norm, 'params': ['loc', 'scale']},
+    'Poisson': {'func': stats.poisson, 'params': ['mu']},
+    'T': {'func': stats.t, 'params': ['df']},
+    'Uniform': {'func': stats.uniform, 'params': ['loc', 'scale']},
+    'Weibull': {'func': stats.weibull_min, 'params': ['c', 'scale']}
 }
 
 # Define a function to calculate the probability density/mass function
-def calc_prob_dist(distribution, params, prob_type, x1=None, x2=None):
-    dist = distributions[distribution](*params)
-    
-    if prob_type == 'P(X < x)':
-        prob = dist.cdf(x1)
-    elif prob_type == 'P(X > x)':
-        prob = 1 - dist.cdf(x1)
-    elif prob_type == 'P(x1 < X < x2)':
-        prob = dist.cdf(x2) - dist.cdf(x1)
-    
+def calc_prob_dist(distribution, params, x, case):
+    dist = distributions[distribution]['func'](*params)
+    if case == 'p<x':
+        prob = dist.cdf(x)
+    elif case == 'p>x':
+        prob = 1 - dist.cdf(x)
+    elif case == 'p1<x<p2':
+        prob = dist.cdf(x[1]) - dist.cdf(x[0])
     return prob
 
 # Define the Streamlit app
 def main():
     st.title('Probability Calculator')
-    st.write('This app allows you to calculate probabilities of different distributions.')
+    st.write('This app allows you to calculate the probability density/mass function of different distributions.')
     
     # Create dropdown menu to select a distribution
     distribution = st.selectbox('Select a distribution:', list(distributions.keys()))
     
-    # Get parameters for the selected distribution
+    # Get parameter values for the selected distribution
     params = []
-    for param in distributions[distribution].shapes:
+    for param in distributions[distribution]['params']:
         param_val = st.number_input(f'Enter value for {param}:', value=1)
         params.append(param_val)
     
-    # Create radio button to select the type of probability calculation
-    prob_type = st.radio('Select the type of probability calculation:',
-                         ['P(X < x)', 'P(X > x)', 'P(x1 < X < x2)'])
+    # Create dropdown menu to select the case
+    case = st.selectbox('Select a case:', ['p<x', 'p>x', 'p1<x<p2'])
     
-    # Get x values for the probability calculation (if applicable)
-    if prob_type == 'P(X < x)' or prob_type == 'P(X > x)':
-        x1 = st.number_input('Enter x value:', value=0)
-        x2 = None
-    else:
-        x1 = st.number_input('Enter x1 value:', value=0)
-        x2 = st.number_input('Enter x2 value:', value=1)
+    # Get x value(s) for the probability density/mass function
+    if case == 'p<x':
+        x = st.number_input('Enter x value:', value=0.5)
+    elif case == 'p>x':
+        x = st.number_input('Enter x value:', value=0.5)
+    elif case == 'p1<x<p2':
+        x_min = st.number_input('Enter minimum x value:', value=0)
+        x_max = st.number_input('Enter maximum x value:', value=1)
+        x = [x_min, x_max]
     
     # Calculate the probability
-    prob = calc_prob_dist(distribution, params, prob_type, x1, x2)
+    prob = calc_prob_dist(distribution, params, x, case)
     
-    # Display the result
-    st.write(f'{prob_type} = {prob:.4f}')
+    # Display the probability
+    st.write(f'P({case}) = {prob}')
+    
 
 if __name__ == '__main__':
     main()
-
