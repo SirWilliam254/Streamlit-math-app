73ca121d2ee7fff4083a2c7bc00b535c059382e7
William Mburu <69644126+SirWilliam254@users.noreply.github.com>
2023-03-03 03:30:11 +0300

    Add files via upload

--- pages/6_📈_MatrixMultplication.py
+++ pages/6_📈_MatrixMultplication.py
@@ -12,23 +12,19 @@ def write_csv(matrix):
     csv_string = df.to_csv(index=False, sep='\t', header=False, encoding='utf-8-sig')
     return csv_string.encode('utf-8')
 
-def is_numeric(matrix):
-    # Check if all variables in the matrix are numeric
-    try:
-        matrix.astype(float)
-        return True
-    except ValueError:
-        return False
-
 def multiply_matrices(matrix1, matrix2):
     if matrix1.shape[1] != matrix2.shape[0]:
         st.warning("Matrices cannot be multiplied due to incompatible dimensions.")
         return None
     else:
-        return np.matmul(matrix1, matrix2)
+        result = np.zeros((matrix1.shape[0], matrix2.shape[1]))
+        for i in range(matrix1.shape[0]):
+            for j in range(matrix2.shape[1]):
+                for k in range(matrix1.shape[1]):
+                    result[i][j] += matrix1[i][k] * matrix2[k][j]
+        return result
 
 st.title("Matrix Multiplication App")
-st.write("""Upload only csv data that does not contain missing values and also no title rows, just matrix data in each cell from cell A1""")
 
 file1 = st.file_uploader("Upload CSV file for matrix 1:", type=["csv"])
 file2 = st.file_uploader("Upload CSV file for matrix 2:", type=["csv"])
@@ -36,19 +32,23 @@ file2 = st.file_uploader("Upload CSV file for matrix 2:", type=["csv"])
 if file1 is not None and file2 is not None:
     st.write("Matrix 1:")
     matrix1 = read_csv(file1)
-    if not is_numeric(matrix1):
-        st.warning("Matrix 1 contains non-numeric values.")
-        st.stop()
     st.write(matrix1)
 
     st.write("Matrix 2:")
     matrix2 = read_csv(file2)
-    if not is_numeric(matrix2):
-        st.warning("Matrix 2 contains non-numeric values.")
-        st.stop()
     st.write(matrix2)
 
     if st.button("Multiply Matrices"):
+        st.write("Multiplication Process:")
+        process = np.zeros((matrix1.shape[0], matrix2.shape[1]), dtype=object)
+        for i in range(matrix1.shape[0]):
+            for j in range(matrix2.shape[1]):
+                temp = []
+                for k in range(matrix1.shape[1]):
+                    temp.append(f"{matrix1[i][k]} x {matrix2[k][j]}")
+                process[i][j] = ' + '.join(temp)
+        st.write(process)
+        
         st.write("Resultant Matrix:")
         result = multiply_matrices(matrix1, matrix2)
         if result is not None:
